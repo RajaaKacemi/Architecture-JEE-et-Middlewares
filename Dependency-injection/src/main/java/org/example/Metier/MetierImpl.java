0054000ee1bae540c5e8ae755c011864743a2477
RajaaKacemi <rajaa01.kacemi@gmail.com>
2025-03-07 23:02:00 +0000

    Injection via constructor

--- Dependency-injection/src/main/java/org/example/Metier/MetierImpl.java
+++ Dependency-injection/src/main/java/org/example/Metier/MetierImpl.java
@@ -1,16 +1,26 @@
 package org.example.Metier;
 
+import org.example.Dao.DaoImpl;
 import org.example.Dao.IDao;
 
 public class MetierImpl implements IMetier {
 
+    /**
+     * to inject an object into the dao variable
+     * of a class that implements the IDao interface
+     */
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
     // Couplage Faible
-    private IDao dao;
+    private IDao dao = null;
 
     @Override
     public double calcul() {
         double T = dao.getData() * 23;
         return T;
     }
+
 
 }
--- Dependency-injection/src/main/java/org/example/Presentation/Presentation.java
+++ Dependency-injection/src/main/java/org/example/Presentation/Presentation.java
@@ -1,4 +1,18 @@
 package org.example.Presentation;
 
+import org.example.Dao.DaoImpl;
+import org.example.Metier.MetierImpl;
+
 public class Presentation {
+    public static void main(String[] args) {
+        /**
+         * Injection of dependencies by static instantiation
+         */
+        DaoImpl dao = new DaoImpl();
+        /**
+         * Injection via constructor
+         */
+        MetierImpl metier = new MetierImpl(dao);
+        System.out.println(metier.calcul());
+    }
 }
